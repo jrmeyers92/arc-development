9ec1de13cccc9a42aaf9d5e2eec565f7dab625df
Jake Meyers <jakejake92@yahoo.com>
2020-12-03 16:33:23 -0700

    header ui done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/styles";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./ui/Header";
 import theme from "./ui/Theme";
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<Header />
-			hello
+			<BrowserRouter>
+				<Header />
+				<Switch>
+					<Route
+						exact
+						path='/'
+						component={() => {
+							<div>Home</div>;
+						}}
+					/>
+				</Switch>
+			</BrowserRouter>
 		</ThemeProvider>
 	);
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import Toolbar from "@material-ui/core/Toolbar";
+import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
 	const { children } = props;
@@ -16,12 +22,65 @@ function ElevationScroll(props) {
 	});
 }
 
+const useStyles = makeStyles((theme) => ({
+	toolbarMargin: {
+		...theme.mixins.Toolbar,
+		marginBottom: "7.5em",
+	},
+	logo: {
+		height: "7em",
+	},
+	tabContainer: {
+		marginLeft: "auto",
+	},
+	tab: {
+		...theme.typography.tab,
+		minWidth: 10,
+		marginLeft: "25px",
+	},
+	button: {
+		borderRadius: "50px",
+		marginLeft: "50px",
+		marginRight: "25px",
+		height: "45px",
+		...theme.typography.estimate,
+	},
+}));
+
 export default function Header(props) {
+	const classes = useStyles();
+	const [value, setValue] = useState(0);
+
+	const handleChange = (e, value) => {
+		setValue(value);
+	};
 	return (
-		<ElevationScroll>
-			<AppBar position='fixed'>
-				<Toolbar>Arc Developement</Toolbar>
-			</AppBar>
-		</ElevationScroll>
+		<React.Fragment>
+			<ElevationScroll>
+				<AppBar position='fixed' color='primary'>
+					<Toolbar disableGutters>
+						<img src={logo} alt='company logo' className={classes.logo}></img>
+						<Tabs
+							value={value}
+							onChange={handleChange}
+							indicatorColor='primary'
+							className={classes.tabContainer}>
+							<Tab label='Home' className={classes.tab} />
+							<Tab label='Services' className={classes.tab} />
+							<Tab label='The Revolution' className={classes.tab} />
+							<Tab label='About Us' className={classes.tab} />
+							<Tab label='Contact Us' className={classes.tab} />
+						</Tabs>
+						<Button
+							variant='contained'
+							color='secondary'
+							className={classes.button}>
+							Free Estimate
+						</Button>
+					</Toolbar>
+				</AppBar>
+			</ElevationScroll>
+			<div className={classes.toolbarMargin} />
+		</React.Fragment>
 	);
 }
