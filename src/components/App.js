769564ddc3821be6a00ba1ea9c52a484260ed8ce
Jake Meyers <jakejake92@yahoo.com>
2020-12-02 19:56:43 -0700

    started themeing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { ThemeProvider } from "@material-ui/styles";
 import Header from "./ui/Header";
+import theme from "./ui/Theme";
 
 function App() {
 	return (
-		<div className='App'>
+		<ThemeProvider theme={theme}>
 			<Header />
 			hello
-		</div>
+		</ThemeProvider>
 	);
 }
 
---
+++ src/components/ui/Theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const arcBlue = "#0B72b9"; 
+const arcOrange = "#FFBA60",
+
+const theme = createMuiTheme({
+ return  palette: {
+    primary: `${arcBlue}`,
+  },
+});
+
+export default theme;
