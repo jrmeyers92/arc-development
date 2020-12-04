a46dcfaedc162ddb088bf7e5cec5ea00ff84a039
Jake Meyers <jakejake92@yahoo.com>
2020-12-03 17:02:11 -0700

    progress on nav header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -29,7 +29,13 @@ const useStyles = makeStyles((theme) => ({
 		marginBottom: "7.5em",
 	},
 	logo: {
-		height: "7em",
+		height: "8em",
+	},
+	logoContainer: {
+		padding: 0,
+		"&:hover": {
+			backgroundColor: "transparent",
+		},
 	},
 	tabContainer: {
 		marginLeft: "auto",
@@ -55,12 +61,39 @@ export default function Header(props) {
 	const handleChange = (e, value) => {
 		setValue(value);
 	};
+
+	useEffect(() => {
+		if (window.location.pathname === "/" && value !== 0) {
+			setValue(0);
+		} else if (window.location.pathname === "/services" && value !== 1) {
+			setValue(1);
+		} else if (window.location.pathname === "/revolution" && value !== 2) {
+			setValue(2);
+		} else if (window.location.pathname === "/about" && value !== 3) {
+			setValue(3);
+		} else if (window.location.pathname === "/contact" && value !== 4) {
+			setValue(4);
+		} else if (window.location.pathname === "/estimate" && value !== 5) {
+			setValue(5);
+		}
+	}, [value]);
+
 	return (
 		<React.Fragment>
 			<ElevationScroll>
 				<AppBar position='fixed' color='primary'>
 					<Toolbar disableGutters>
-						<img src={logo} alt='company logo' className={classes.logo}></img>
+						<Button
+							component={Link}
+							to='/'
+							disableRipple
+							className={classes.logoContainer}
+							onClick={() => {
+								setValue(0);
+							}}>
+							<img src={logo} alt='company logo' className={classes.logo}></img>
+						</Button>
+
 						<Tabs
 							value={value}
 							onChange={handleChange}
@@ -100,7 +133,9 @@ export default function Header(props) {
 						<Button
 							variant='contained'
 							color='secondary'
-							className={classes.button}>
+							className={classes.button}
+							component={Link}
+							to='estimate'>
 							Free Estimate
 						</Button>
 					</Toolbar>
